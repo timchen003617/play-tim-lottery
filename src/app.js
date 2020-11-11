d136f2eb969e61298d253dd5585848d3fdae9887
timchen <gn02321555@gmail.com>
2020-11-11 16:05:07 +0800

    test

--- src/app.js
+++ src/app.js
@@ -27,16 +27,23 @@ app.use(cors())
 
 const jsonParser = bodyParser.json({ limit: '8mb' })
 
-
-let folderName = 'client'
-
-app.use(express.static(join(config.root, '..', folderName, 'dist')))
-
 app.use('/api', bodyParser.urlencoded({ extended: false }))
 app.use('/api', jsonParser, routes)
 
-app.get('/*', (req, res) => {
-  res.sendFile(join(config.root, '..', folderName, 'dist', 'index.html'))
+// the __dirname is the current directory from where the script is running
+app.use(express.static(join(__dirname, 'client/dist')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, 'client/dist')));
+  //
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname = 'client/dist/index.html'));
+  })
+}
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname = '/client/dist/index.html'));
 })
 
 app.use((err, req, res, next) => {
